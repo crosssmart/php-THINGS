10a82dd2452f5335f1611c9e61d77a527d91e869
Rogério Filho <100595069+crosssmart@users.noreply.github.com>
2023-11-06 11:34:26 -0400

    dasdawd
    asdawd

---
+++ formulario/cad.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+    <main>
+        <?php
+        $n = $_GET ["nome"];
+        $s = $_GET ["sobrenome"];
+
+        echo "<p> É um prazer te conhecer, <strong>$n $s</strong> !
+        Este é meu site!</p>"
+
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para página anterior</a></p>
+    </main>
+</body>
+</html>
